2dd0438ad55535e02dc4d19f76dcfff84a8ad5b0
Malo-one2000 <84922280+Malo-one2000@users.noreply.github.com>
2022-12-06 18:05:08 +0000

    trying help embed

--- JavaScript/commands/help.js
+++ JavaScript/commands/help.js
@@ -1,21 +1,14 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+// inside a command, event listener, etc.
+const helpEmbed = new EmbedBuilder()
+	.setColor(0x0099FF)
+	.setTitle('Help')
+	.setDescription('A menu with all of the commands and some more context.')
+	.addFields(
+		{ name: 'Ping', value: 'Sends back with a pong.', inline: true},
+		{ name: 'User', value: 'Tells you a bit about the user. ', inline: true },
+		{ name: 'Server', value: 'Tells you a bit more about the server.', inline: true },
+	)
+	.setTimestamp()
+	.setFooter({ text: 'Thanks for using my bot'});
 
-module.exports = {
-    data: new SlashCommandBuilder()
-    .setName('help')
-    .setDescription('Lists all available commands'),
-    async execute(interaction) {
-        let str
-        const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-        for (const file of commandFiles) {
-        const command = require(`./${file}`);
-        str += `Name: ${command.data.name}, Description: ${command.data.description} \n`;
-        }
-
-        return interaction.reply({
-        content: str,
-        ephemeral: true,
-        });
-    },
-};
+channel.send({ embeds: [helpEmbed] });
